ec4a855d1c27eb458fec778af0a12ce891654dd6
Anonymous <sardorxon18>
2024-10-09 22:55:53 +0500

    static file fixed

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -119,10 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'staticfiles',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
 STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
